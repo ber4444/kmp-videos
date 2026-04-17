4221f49f7bf5ba42b74b32c514d65bf68a59e6d1
gabor berenyi <205867+ber4444@users.noreply.github.com>
2026-04-16 18:53:00 -0700

    fix android

--- composeApp/src/androidMain/kotlin/com/livingpresence/inner/circle/squared/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/com/livingpresence/inner/circle/squared/Platform.android.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -17,7 +18,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
